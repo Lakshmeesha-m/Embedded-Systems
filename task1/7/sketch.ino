15cde02e261defd0fdf9f8f4bfa7f1797e0193df
Lakshmeesha M <mlakshmeesha07@gmail.com>
2026-01-22 10:44:07 +0530

    Add files via upload

---
+++ task1/7/sketch.ino
@@ -0,0 +1,21 @@
+#define IRdata 9
+#define LED 2
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(IRdata, INPUT);
+  pinMode(LED, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(IRdata)==LOW)
+  {
+    digitalWrite(LED,HIGH);
+  }
+  else
+  {
+    digitalWrite(LED,LOW);
+  }
+
+}
